680cd68d97d0cfa8e9ec7d6a6b387a4a00233086
Jesus Briales <jesusbriales@gmail.com>
2014-09-09 13:15:22 +0200

    Changed CSimRig to use Lidar pose as base
    Absolute poses are stored in properties Camera and Lidar but not as properties of CSimRig

--- Simulation/CSimRig.m
+++ Simulation/CSimRig.m
@@ -3,16 +3,15 @@ classdef CSimRig < handle
     % This class stores configuration parameters and pose and ease methods
     % to simulate Lidar and Camera measurements of a pattern
     %   Constructor:
-    %   Rig = CSimRig( R_w_c, t_w_c, R_c_s, t_c_s,... % Extrinsic options
+    %   Rig = CSimRig( R_w_s, t_w_s, R_c_s, t_c_s,... % Extrinsic options
     %                  N, FOVd, scan_sd, d_range,... % Lidar options
     %                  K, res, f, cam_sd ); % Camera options
+    %   Important: The given absolute pose is for Lidar from World
     % See also: CSimLidar, CCamLidar.
     
     properties
         Camera  % Simulated Camera object
         Lidar   % Simulated Lidar object
-        R_w_c   % Absolute rotation of Camera seen from World
-        t_w_c   % Absolute translation of Camera seen from World
         R_c_s   % Relative rotation of Lidar seen from Camera
         t_c_s   % Relative translation of Lidar seen from Camera
     end
@@ -20,28 +19,29 @@ classdef CSimRig < handle
     methods
         % Constructor
         function obj = ...
-            CSimRig( R_w_c, t_w_c, R_c_s, t_c_s,... % Extrinsic options
+            CSimRig( R_w_s, t_w_s, R_c_s, t_c_s,... % Extrinsic options
                      N, FOVd, scan_sd, d_range,... % Lidar options
                      K, res, f, cam_sd ) % Camera options
-            obj.R_w_c  = R_w_c;
-            obj.t_w_c  = t_w_c;
             obj.R_c_s  = R_c_s;
             obj.t_c_s  = t_c_s;
-            R_w_s = R_w_c * R_c_s;
-            t_w_s = t_w_c + R_w_c * t_c_s;
+            R_w_c = R_w_s * R_c_s';
+            t_w_c = t_w_s - R_w_c * t_c_s;
             
-            obj.Camera = CSimCamera( R_w_c, t_w_c, K, res, f, cam_sd );
-            obj.Lidar  = CSimLidar( R_w_s, t_w_s, N, FOVd, scan_sd, d_range ); 
+            obj.Camera = CSimCamera( R_w_c, t_w_c,...
+                                     K, res, f, cam_sd );
+            obj.Lidar  = CSimLidar( R_w_s, t_w_s,...
+                                    N, FOVd, scan_sd, d_range ); 
         end
         
         % Update poses
-        function obj = updatePose( obj, R_w_c, t_w_c )
-            obj.Camera.R = R_w_c;
-            obj.Camera.t = t_w_c;
-            R_w_s = R_w_c * obj.R_c_s;
-            t_w_s = t_w_c + R_w_c * obj.t_c_s;
+        function obj = updatePose( obj, R_w_s, t_w_s )
             obj.Lidar.R = R_w_s;
             obj.Lidar.t = t_w_s;
+            
+            R_w_c = R_w_s * obj.R_c_s';
+            t_w_c = t_w_s - R_w_c * obj.t_c_s;
+            obj.Camera.R = R_w_c;
+            obj.Camera.t = t_w_c;
         end
     end
     
