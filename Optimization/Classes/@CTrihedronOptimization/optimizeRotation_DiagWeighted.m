33d7cd0b0aa880436d5a5fd6da910fd2bdf562e3
Jesus Briales <jesusbriales@gmail.com>
2014-09-12 09:04:38 +0200

    New Rotation optimization function in which only diagonal of weights matrix are used

---
+++ Optimization/Classes/@CTrihedronOptimization/optimizeRotation_DiagWeighted.m
@@ -0,0 +1,17 @@
+function R = optimizeRotation_DiagWeighted( obj )
+% Function to optimize with LM the Rotation wrt given correspondences
+
+R0 = obj.R0;
+[ R, err, errNorm, W ] = LM_Man_optim(@LM_Fun,R0,...
+            'space','SO(3)','weighted',true,'debug',obj.debug_level, 'maxIters', 200);
+
+obj.R = R;
+        
+    function [residual, jacobian, weights] = LM_Fun( R )
+        residual = obj.FErr_Orthogonality( R );
+        jacobian = obj.FJac_Orthogonality( R );
+        weights  = obj.FWeights_Orthogonality( R );
+        weights  = diag( diag(weights) );
+    end
+
+end
