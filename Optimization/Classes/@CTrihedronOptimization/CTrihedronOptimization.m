d61387ba273b38c2a90ea200321b5686a376435d
rubengooj <rubengooj@gmail.com>
2014-09-12 09:45:29 +0200

    Changes in main_Simulation3

--- Optimization/Classes/@CTrihedronOptimization/CTrihedronOptimization.m
+++ Optimization/Classes/@CTrihedronOptimization/CTrihedronOptimization.m
@@ -6,7 +6,11 @@ classdef CTrihedronOptimization < handle
     properties
         obs     % Array of CTrihedronObservation
         
+        % Auxiliar variables
+        K   % Camera intrinsic matrix (for translation optimization)
+        
         RANSAC_Rotation_threshold % Threshold for rotation error function
+        RANSAC_Translation_threshold % Threshold for translation error function
         debug_level % Verbose level when optimizing
     end
     
@@ -34,7 +38,7 @@ classdef CTrihedronOptimization < handle
     
     methods
         %% Constructor
-        function obj = CTrihedronOptimization( RANSAC_Rotation_threshold )
+        function obj = CTrihedronOptimization( K, RANSAC_Rotation_threshold, RANSAC_Translation_threshold, debug_level )
             obj.obs = CTrihedronObservation.empty(1,0);
             
             if ~exist('RANSAC_Rotation_threshold','var')
@@ -42,10 +46,17 @@ classdef CTrihedronOptimization < handle
             end
             obj.RANSAC_Rotation_threshold = RANSAC_Rotation_threshold;
             
+            if ~exist('RANSAC_Translation_threshold','var')
+                RANSAC_Translation_threshold = 1e-3;
+            end
+            obj.RANSAC_Translation_threshold = RANSAC_Translation_threshold;
+            
             if ~exist('debug_level','var')
                 debug_level = 2;
             end
             obj.debug_level = debug_level;
+            
+            obj.K = K;
             
             % Commented because is dependent now
 %             obj.mask_RANSAC_R_outliers = []; % Initialize as empty
@@ -62,14 +73,44 @@ classdef CTrihedronOptimization < handle
         
         %% Filter correspondences with RANSAC
         obj = filterRotationRANSAC( obj )
+        obj = filterTranslationRANSAC( obj )
+        % Functions to set all outlier masks to false
+        function obj = resetRotationRANSAC( obj )
+            Nobs = length( obj.obs );
+            for i=1:Nobs
+                obj.obs(i).is_R_outlier = false(1,3);
+            end
+        end
+        function obj = resetTranslationRANSAC( obj )
+            Nobs = length( obj.obs );
+            for i=1:Nobs
+                obj.obs(i).is_t_outlier = false(1,3);
+            end
+        end
         
         %% Compute initial estimate
         function obj = setInitialRotation( obj, R0 )
             obj.R0 = R0;
         end
+        
+        function obj = setInitialTranslation( obj, t0 )
+            obj.t0 = t0;
+        end
+        
+        function obj = computeTranslationLinear( obj )
+            L = obj.cam_L;
+            q = obj.LRF_Q;
+            
+            b = - dot( L, obj.R(:,1:2) * q, 1 )';
+            A = L';
+            t_lin = A \ b;
+            
+            obj.t0 = t_lin;
+        end
         % TODO: Automate from complete poses
         
         %% Optimization functions and methods
+        % For Rotation
         function residual = FErr_Orthogonality( obj, R )
             % Compute error vector for observations data and certain R
             n_cam = obj.cam_N;
@@ -124,21 +165,31 @@ classdef CTrihedronOptimization < handle
                 JN = blkdiag( RL{:} )'; % Derivative of e_XYZ wrt N_XYZ
                 Ja = diag( dot( N, R12*ort*V ) ); 
                 A_i = JN * A_N * JN' + Ja * A_V * Ja';
-                W{i} = pinv( A_i );
+                if any(A_i(:))
+                    W{i} = pinv( A_i );
+                else
+                    W{i} = eye(length(A_i))
+                end
             end
             weights = blkdiag( W{:} );
         end
         
         R = optimizeRotation_NonWeighted( obj )
         R = optimizeRotation_Weighted( obj )
+        R = optimizeRotation_DiagWeighted( obj )
         J = FJac_optimizeRotation_NonWeighted( obj, R )
         
         % TODO: Implement optimizeRotation_Covariance from optimRotation.m
         
         plot( obj )
         
+        % For Translation
+        t = optimizeTranslation_2D_NonWeighted( obj )
+        t = optimizeTranslation_2D_Weighted( obj )
+        
         
         %% Get-functions
+        % For rotation
         function cam_N = get.cam_N( obj )
             cam_N = [obj.obs.cam_R_c_w];
             % Mask with existing measures and not-inliers
@@ -154,6 +205,24 @@ classdef CTrihedronOptimization < handle
             LRF_V( ~mask_valid ) = [];
             % Convert from cell with empty elements to dense matrix
             LRF_V = cell2mat( LRF_V );
+        end
+        
+        % For translation
+        function cam_L = get.cam_L( obj )
+            cam_L = [obj.obs.cam_l];
+            % Mask with existing measures and not-inliers
+            mask_valid = obj.mask_LRF_Q & (~obj.mask_RANSAC_t_outliers);
+            cam_L = cam_L(:, mask_valid);
+        end
+        
+        function LRF_Q = get.LRF_Q( obj )
+            LRF_Q = [obj.obs.LRF_q];
+            % Remove non-observed data: Not necessary really (already empty)
+            % Remove outliers: Set as empty observations
+            mask_valid = obj.mask_LRF_Q & (~obj.mask_RANSAC_t_outliers);
+            LRF_Q( ~mask_valid ) = [];
+            % Convert from cell with empty elements to dense matrix
+            LRF_Q = cell2mat( LRF_Q );
         end
         
         function mask_LRF_V = get.mask_LRF_V( obj )
---
+++ Optimization/Classes/@CTrihedronOptimization/filterTranslationRANSAC.m
@@ -0,0 +1,69 @@
+function obj = filterTranslationRANSAC( obj )
+
+% Mask of existing correspondences
+mask_exist = obj.mask_LRF_Q;
+idxs_exist = find( mask_exist );
+
+% Normalize P2 homogeneous lines to plane normals
+N = obj.cam_L;
+N = N ./ repmat( sqrt(sum(N.^2,1)) , 3,1);
+corresps = [ N ; obj.LRF_Q ];
+feedback = true;
+
+s = 3;  % Minimum No of points needed to define rotation.
+        
+fittingfn = @computeTranslationLinear;
+distfn    = @correspdist;
+degenfn   = @isdegenerate;
+
+[t0, inliers] = ransac(corresps, fittingfn, distfn, degenfn, s, obj.RANSAC_Translation_threshold, feedback);
+   
+% R0 can be used as initial estimate for optimization process
+obj.t0 = t0;
+
+% Find indexes of outliers in complete set of 3xN correspondences
+idxs_inliers  = idxs_exist( inliers );
+idxs_outliers = setdiff( idxs_exist, idxs_inliers );
+
+%% Assign outlier tag to each observation obs(i)
+Nobs = length( obj.obs );
+map = reshape( 1:3*Nobs, 3,Nobs );
+
+% Assign 1 to elements which are outliers
+mask_RANSAC_t_outliers = false(1,length(mask_exist));
+mask_RANSAC_t_outliers( idxs_outliers ) = true;
+for i=1:Nobs
+    obj.obs(i).is_t_outlier = mask_RANSAC_t_outliers( map(:,i) );
+end
+
+%% RANSAC auxiliary functions
+function t = computeTranslationLinear( X )
+L = X(1:3,:);
+Q = X(4:5,:);
+
+b = - dot( L, obj.R(:,1:2) * Q, 1 )';
+A = L';
+t = A \ b;
+end
+
+function [inliers, t] = correspdist(t, X, thres)
+    
+    all_n = X(1:3,:);
+    all_q = X(4:5,:);
+    Ncorresps = size(X,2);
+    
+    d = dot( all_n, obj.R(:,1:2)*all_q + repmat(t,1,Ncorresps) );
+    
+    inliers = find(abs(d) < thres);
+end
+
+function r = isdegenerate(X)
+    % TODO: Degeneracy (rank) should be avoided to get good estimate
+%     disp( X(1:3,:) )
+%     fprintf('Det  = %f\n', abs( det(X(1:3,:)) ));
+%     fprintf('Cond = %f\n', cond( X(1:3,:) ));
+%     r = false;
+    r = ( abs(det(X(1:3,:))) < 1e-3 ); % Best value seems to be 1e-2
+end
+
+end
