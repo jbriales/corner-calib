276e02df0c5b31cd1d078321e250ab5556ab02fb
Jesus Briales <jesusbriales@gmail.com>
2014-09-13 04:59:59 +0200

    More adds related to previous commit

--- Optimization/Classes/@CTrihedronOptimization/optimizeRotation_DiagWeighted.m
+++ Optimization/Classes/@CTrihedronOptimization/optimizeRotation_DiagWeighted.m
@@ -4,8 +4,6 @@ function R = optimizeRotation_DiagWeighted( obj )
 R0 = obj.R0;
 [ R, err, errNorm, W ] = LM_Man_optim(@LM_Fun,R0,...
             'space','SO(3)','weighted',true,'debug',obj.debug_level, 'maxIters', obj.maxIters);
-
-obj.R = R;
         
     function [residual, jacobian, weights] = LM_Fun( R )
         residual = obj.FErr_Orthogonality( R );
---
+++ Optimization/Classes/@CTrihedronOptimization/plotRotationCostFunction.m
@@ -0,0 +1,49 @@
+function h = plotRotationCostFunction( obj, R )
+dist  = 0.005;    % Simulation distance (rad)
+inc   = 0.0005;   % Increments
+
+gv  = -dist:inc:+dist;
+Ngv = length(gv);
+[w_i,w_j] = meshgrid( gv );
+
+% Linearize code
+inc_eps = [ w_i(:), w_j(:), zeros(Ngv^2,1) ]';
+
+weights = obj.FWeights_Orthogonality( R );
+
+labels = { 'x', 'y', 'z'
+    'y', 'z', 'x' };
+titles = { 'X-Y err^2', 'Y-Z err^2', 'Z-X err^2'
+    'X-Y errW' , 'Y-Z errW' , 'Z-X errW' };
+figure, hold on
+for k=1:3
+    Err2 = zeros(1,Ngv^2);
+    ErrW = zeros(1,Ngv^2);
+    inc_eps_ = circshift( inc_eps, k-1 ); % Shift inc_eps rows
+    for i=1:numel(Err2)
+        R_ = expmap( inc_eps_(:,i) ) * R;
+        residual = obj.FErr_Orthogonality( R_ );
+        Err2(i) = residual' * residual;
+        ErrW(i) = residual' * weights * residual;
+    end
+    Err2 = reshape(Err2,Ngv,Ngv);
+    ErrW = reshape(ErrW,Ngv,Ngv);
+    Err = {Err2, ErrW};
+    
+    res_GT = obj.FErr_Orthogonality( R );
+    err2_GT = { res_GT'*res_GT , res_GT'*weights*res_GT };
+    for k_sub=1:2
+        subplot(2,3,k +(k_sub-1)*3 ); hold on;
+        xlabel(labels{1,k});
+        ylabel(labels{2,k});
+        title( titles{k_sub,k} );
+        surf(w_i,w_j, Err{k_sub});
+        % Plot GT point
+        plot3(0,0,err2_GT{k_sub}, '.y', 'LineWidth', 3);
+        axis([-dist +dist -dist +dist]);
+        shading interp;
+        view([90 90]);
+    end
+end
+h = []; % TODO
+end
