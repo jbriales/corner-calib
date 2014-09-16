904fe8371613adca09c1512f125351b43bba2b31
Jesus Briales <jesusbriales@gmail.com>
2014-09-16 18:40:30 +0200

    Changed name (mistake) from Weighted to Wasielewski (using only corner)
    - Add also indexes from properties

--- Comparison/CKwakComp.m
+++ Comparison/CKwakComp.m
@@ -1,9 +1,9 @@
-classdef CKwakComp < handle
+classdef CKwakComp < handle & CBaseComp
     
     properties
         NonWeighted
         ConstWeighted
-        Weighted
+        Wasielewski
     end
     
     methods
@@ -12,10 +12,15 @@ classdef CKwakComp < handle
         function obj = CKwakComp (Nsim, cam_sd_N, scan_sd_N, N_co_N)
             obj.NonWeighted     = cell(cam_sd_N, scan_sd_N, N_co_N, Nsim);
             obj.ConstWeighted   = cell(cam_sd_N, scan_sd_N, N_co_N, Nsim);
-            obj.Weighted        = cell(cam_sd_N, scan_sd_N, N_co_N, Nsim);
+            obj.Wasielewski     = cell(cam_sd_N, scan_sd_N, N_co_N, Nsim);
         end
         
-        function optim( obj, cornerOptim, cam_sd_N_it, scan_sd_N_it, N_co_N_it, Nsim_it )
+        function optim( obj, cornerOptim )
+            % Set indexes for current optimization
+            cam_sd_N_it = obj.idx_cam_sd;
+            scan_sd_N_it = obj.idx_scan_sd;
+            N_co_N_it = obj.idx_N_co;
+            Nsim_it = obj.idx_Nsim;
             
             [R_k_nw, t_k_nw] = cornerOptim.optimizeRt_NonWeighted;
             [R_k_cw, t_k_cw] = cornerOptim.optimizeRt_ConstWeighted;
@@ -23,8 +28,7 @@ classdef CKwakComp < handle
                         
             obj.NonWeighted(cam_sd_N_it, scan_sd_N_it, N_co_N_it, Nsim_it)   = {[R_k_nw  t_k_nw]};
             obj.ConstWeighted(cam_sd_N_it, scan_sd_N_it, N_co_N_it, Nsim_it) = {[R_k_cw  t_k_cw]};
-            obj.Weighted(cam_sd_N_it, scan_sd_N_it, N_co_N_it, Nsim_it)      = {[R_kC_nw t_kC_nw]};
-            
+            obj.Wasielewski(cam_sd_N_it, scan_sd_N_it, N_co_N_it, Nsim_it)   = {[R_kC_nw t_kC_nw]};
         end
         
     end
