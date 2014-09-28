fa14ef6157919bc910064d8a363d5caded0bf05a
Jesus Briales <jesusbriales@gmail.com>
2014-09-28 15:15:36 +0200

    Refactor of CTrihedron class
    - Make some refactor to manifold classes
    - Remove deprecated external methods with stable implementations
    - Rest some Monte Carlo lines (TODO)

--- Simulation/@CTrihedron/CTrihedron.m
+++ Simulation/@CTrihedron/CTrihedron.m
@@ -123,11 +123,7 @@ classdef CTrihedron < CPattern
             % Control variables
             sigma = sd;
             A_cp  = sigma * eye(8);
-            
-            % Define the x and A_x structures
-            x   = zeros(5,1);
-            A_x = zeros(5,5,1);
-            
+                        
             % Assignment of auxiliar variables
             c     = img_pts(:,1);
             delta = img_pts(:,2:4) - repmat(c,1,3);
@@ -137,158 +133,18 @@ classdef CTrihedron < CPattern
             v = Manifold.S1.empty(0,3);
             for i=1:3
                 v(i) = Manifold.S1( delta(:,i) );
-                J_normalize = 1/norm(delta(:,i))^3 * ...
-                    (eye(2) - delta(:,i)*delta(:,i)');
-                J_v__{i} = v(i).DLie * J_normalize;
+                J_normalize = Dsnormalize( delta(:,i) );
+                J_v__{i} = v(i).Dproj * J_normalize;
             end
             J = [ eye(2)            , zeros(2,3*2)      ;
                   -cell2mat(J_v__)' , blkdiag(J_v__{:}) ];
             A_xi = J * A_cp * J';
             
             % Store results in object
+            c  = Manifold.Rn( c );
             obj_xi = Cxi( c, v(1), v(2), v(3) );
             obj_xi.setRepresentationCov( A_xi );
-        end
-        
-        function n = fixTrihedronNormal( obj, c, K, v_im )
-            Dproj = DprojectionP2( c );
-            % Obtain null space in which n lies
-            N = null( skew( [v_im;0] ) * [Dproj;zeros(1,3)] * K );
-            
-            % Get associated quadratic form:
-            Q = (Dproj * K * N)' * (eye(2) - v_im*v_im') * (Dproj * K * N);
-            
-            % Diagonalize to obtain solution (in nullspace)
-            [U,S,V] = svd( Q );
-            lambda = V(:,end);
-            
-            % Convert coordinates in nullspace to real vector n
-            n = N * lambda;
-        end
-        
-        function R_tri = getTrihedronNormals_Closed( obj, c, K, v_im, R_gt )
-            Dproj = DprojectionP2( c );
-            
-            % Obtain null spaces (Om) in which n lies (parallel condition)
-            for i=1:3
-                Om{i} = null( skew([v_im(:,i);0]) * [Dproj;zeros(1,3)] * K );
-            end
-            
-            % Compute GT parameters
-%             keyboard
-            if exist('R_gt','var')
-                n_gt = mat2cell(R_gt,3,[1 1 1]);
-                for i=1:3
-                    lambda_gt{i} = Om{i}' * n_gt{i};
-                end
-            end
-                
-            % Compute bilinear forms matrices
-            Q = cell(3,3);
-            q = cell(1,3);
-            for k=1:3
-                temp = num2cell( setdiff(1:3,k) );
-                [i,j] = deal( temp{:} );
-                
-                q{k}   = Om{i}' * Om{j};
-                Q{k,k} = zeros(2,2);
-                Q{i,j} = Om{i}'*Om{j};
-                Q{j,i} = Om{j}'*Om{i};
-            end
-            % Diagonalize bilinear forms
-            [U{1},~,~] = svd( Q{1,2} );
-            [U{2},~,~] = svd( Q{2,3} );
-            [U{3},~,~] = svd( Q{3,1} );
-%             [U_{1},~,~] = svd( Q{1,3} );
-%             [U_{2},~,~] = svd( Q{2,1} );
-%             [U_{3},~,~] = svd( Q{3,2} );
-%             for i=1:3 % Check equal matrices
-%                 if ~isequal(U{i},U_{i})
-%                     warning('Different U decompositions');
-%                 end
-%             end
-            D = cell(1,3);
-            d = zeros(1,3);
-            for k=1:3
-                temp = num2cell( setdiff(1:3,k) );
-                [i,j] = deal( temp{:} );
-                D{k} = U{i}'*Q{i,j}*U{j};
-                % Is possible to change sign without more concern?
-                D{k} = D{k} * sign(D{k}(1,1));
-                d(k) = -D{k}(2,2);
-            end
-%             keyboard
-            d_m = sqrt( prod(d) );
-            xp = +d_m ./ d;
-            xm = -d_m ./ d;
-            xpm = {xp, xm};
-            for i_sign = 1:2 % Compute with both signs and choose det = 1
-                x = xpm{i_sign};
-                
-                y = ones(1,3);
-                rho = [x ; y];
-                rho = mat2cell( rho, 2,[1 1 1] );
-                lambda = cell(1,3);
-                for k=1:3
-                    lambda{k} = snormalize( U{k} * rho{k} );
-                end
-                % Compute sign of dot product, correct, and normals
-                s = zeros(1,3);
-                n = cell(1,3);
-                for i=1:3
-                    s(i) = sign( v_im(:,i)' * DprojectionP2(c) * K * Om{i} * lambda{i} );
-                    lambda{i} = s(i) * lambda{i};
-                    n{i} = Om{i} * lambda{i};
-                end
-                R_tri = cell2mat( n );
-                
-                if abs( det(R_tri) - 1 ) < 0.5
-                    break
-                end
-            end
-%             keyboard
-            
-            if 0 % Temporal debug (check with other sign)
-                x_ = xm;
-                rho_ = [x_ ; y];
-                rho_ = mat2cell( rho_, 2,[1 1 1] );
-                lambda_ = cell(1,3);
-                for k=1:3
-                    lambda_{k} = snormalize( U{k} * rho_{k} );
-                end
-                % Compute sign of dot product, correct, and normals
-                s_ = zeros(1,3);
-                n_ = cell(1,3);
-                for i=1:3
-                    s_(i) = sign( v_im(:,i)' * DprojectionP2(c) * K * Om{i} * lambda_{i} );
-                    lambda_{i} = s_(i) * lambda_{i};
-                    n_{i} = Om{i} * lambda_{i};
-                end
-                R_tri_ = cell2mat( n_ );
-            end
-            
-            if exist('R_gt','var') % Show results and compare
-                disp( [lambda_gt{:} ; lambda{:}] )
-                disp( [n_gt{:}      ; n{:} ]      )
-                disp( cell2mat(n_gt)-cell2mat(n) )
-                fprintf('Normals equal: %d\n',isequal( n_gt, n ))
-                keyboard
-            end
-            
-            % Debug plotting
-%             keyboard
-%             figure, hold on, rotate3d on, axis equal
-%             plotframe(eye(4), 1, 'W', 'k'); % Trihedron as reference
-%             R_w_c = R_tri';
-% %             R_w_c_ = R_tri_';
-%             t_w_c = -R_w_c * snormalize( K \ [c;1] );
-% %             t_w_c_ = -R_w_c_ * snormalize( K \ [c;1] );
-%             % plotframe([N zeros(3,1); zeros(1,3) 1], 0.5, 'N', 'k', 'LineWidth',3);
-% %             plotframe([R_gt zeros(3,1); zeros(1,3) 1], 0.5, 'gt', 'rgb', 'LineWidth',3);
-%             h1 = plotframe([R_w_c t_w_c; zeros(1,3) 1], 0.5);
-% %             h2 = plotframe([R_w_c_ t_w_c_; zeros(1,3) 1], 0.5);
-%             keyboard
-        end
+        end        
         
         % Get 4x3 cell array with correspondences (lines and points)
         function co = getCorrespondence( obj, Rig )
@@ -296,31 +152,47 @@ classdef CTrihedron < CPattern
             [~, img_pts] = obj.getProjection( Rig.Camera );
             if ~isempty(img_pts)
                 obj_xi  = obj.simulateTOimage( img_pts, Rig.Camera.sd );
+                % Monte Carlo simulated
+                if 0 % Perform Monte Carlo simulation
+                    keyboard
+                    obj_pts = Manifold.Rn( img_pts(:), eye(8) );
+                    out = Manifold.MonteCarloSim( ...,
+                        @(in)obj.simulateTOimage( reshape(in.X,2,4), Rig.Camera.sd ),...
+                        obj_pts, 'Ref', obj_xi, 'N', 1e4 );
+                    keyboard
+                end
+                
                 [obj_Nbp, obj_LP2] = computeBackprojectedNormals( obj_xi, Rig.Camera.K );
-%                 R0 = Rig.Camera.R'; % Initial estimate for R_c_w
+                % Put Monte Carlo simulation here        
+%                 if 1
+%                     obj_S2 = Manifold.S2([2;3;1]);
+%                     obj_S2.setMinimalCov([1 0.5;0.5 1]);
+%                     out = Manifold.MonteCarloSim( ...
+%                         @(X)X, obj_S2, 'Ref', obj_S2, 'N', 1e4 );
+%                     keyboard
+%                 end
+%                 if 1 % Perform Monte Carlo simulation
+%                     out = Manifold.MonteCarloSim( ...
+%                         @(X)auxFunction( X, Rig.Camera.K ),...
+%                         obj_xi, 'Ref', obj_LP2, 'N', 1e2 );
+%                     keyboard
+%                 end
+                % TODO: Check yet
+                if 0 % Perform Monte Carlo simulation
+                    % If computeTrihedronNormals is working correctly and
+                    % its covariance is based on Nbp covariance and values,
+                    % this should be also correct. Maybe there is a mistake
+                    % in Monte Carlo simulation (check S2 manifold
+                    % operations involved in output such as mean, etc.)
+                    keyboard
+                    out = Manifold.MonteCarloSim( ...,
+                        @(X)computeBackprojectedNormals( X, Rig.Camera.K ),...
+                        obj_xi, 'Ref', obj_Nbp, 'N', 1e3 );
+                    keyboard
+                end
                 
-%                 c = obj_xi.c;
-%                 K = Rig.Camera.K;
-%                 v_im = reshape(obj_xi.X(3:end),2,[]);
-%                 R_gt = Rig.Camera.R';
-%                 Rtri = obj.getTrihedronNormals_Closed( c, K, v_im ); % Final GT
-                obj_Rtri = computeTrihedronNormals( obj_xi, obj_Nbp, Rig.Camera.K );
-%                 obj_Rtri = obj.getTrihedronNormals( obj_Nbp, R0 );
-%                 fprintf('Distance: %f\n',angularDistance(R_gt,Rtri));
-%                 disp('Done')
-%                 keyboard
-                
-%                 % Compute covariance
-%                 obj_Rtri = Manifold.SO3( Rtri );
-%                 % Temporarily use the other function
-%                 N = obj_Nbp.arr;
-%                 J_Phi = @(R) cross( R, N, 1 )';
-%                 % Compute covariance of trihedron normals
-%                 J_Phi_eps = J_Phi( Rtri );
-%                 J_Phi_Nbp = mat2cell( Rtri, 3, [1 1 1] );
-%                 J_Phi_Nbp = blkdiag( J_Phi_Nbp{:} )';
-%                 J_eps_Nbp = - J_Phi_eps \ J_Phi_Nbp;
-%                 obj_Rtri.setMinimalCov( J_eps_Nbp * obj_Nbp.A_X * J_eps_Nbp' );
+                obj_Rtri = computeTrihedronNormals( obj_xi, Rig.Camera.K, obj_Nbp );
+                % Checked with Monte Carlo: 0.36% relative error with N=1e5
             else
                 co = [];
                 return
