ce3ab106d1b58427c833e8740708b4cadde313b0
Jesus Briales <jesusbriales@gmail.com>
2014-12-02 23:32:07 +0100

    Ideal cube (8 points) and computation with Elqursh and Trihedron

--- Simulation/CCube.m
+++ Simulation/CCube.m
@@ -7,6 +7,8 @@ classdef CCube < CPattern
     %   p3D  - a 3x7 array with coordinates of points
     
     properties (SetAccess = protected) % (Read-only)
+        C_p3D
+        C_ind % Cell with indexes in linearised cell
         % Empty
     end
     
@@ -29,21 +31,25 @@ classdef CCube < CPattern
             obj.NF = 3;
             % Create faces: 1,2,3 correspond to planes perpendicular to
             % X,Y,Z axes respectively
-            face_R{1} = -[ 0 1 0 ; 0 0 1 ; 1 0 0 ]';
-            face_R{2} = -[ 0 0 1 ; 1 0 0 ; 0 1 0 ]';
-            face_R{3} = -[ 1 0 0 ; 0 1 0 ; 0 0 1 ]';
-            p = [ 0 0 ; L 0 ; L L ; 0 L ]';
-            for i=1:obj.NF
-                obj.face{i} = CPolygon( R * face_R{i}, t, p );
-            end
+%             face_R{1} = -[ 0 1 0 ; 0 0 1 ; 1 0 0 ]';
+%             face_R{2} = -[ 0 0 1 ; 1 0 0 ; 0 1 0 ]';
+%             face_R{3} = -[ 1 0 0 ; 0 1 0 ; 0 0 1 ]';
+%             p = [ 0 0 ; L 0 ; L L ; 0 L ]';
+%             for i=1:obj.NF
+%                 obj.face{i} = CPolygon( R * face_R{i}, t, p );
+%             end
             
-            obj.p3D = - [ 0 0 0 ;
-                        L 0 0 ;
-                        0 L 0 ;
-                        0 0 L ;
-                        L L 0 ;
-                        0 L L ;
-                        L 0 L ]';
+            obj.C_p3D = cell(2,2,2);
+            for i=1:2
+                for j=1:2
+                    for k=1:2
+                        obj.C_p3D{i,j,k}= L/2 * [ (-1)^(i+1)
+                                                  (-1)^(j+1)
+                                                  (-1)^(k+1) ];
+                    end
+                end
+            end
+            obj.p3D = reshape( cell2mat(obj.C_p3D), 3, [] );            
             obj.p3D = makeinhomogeneous( obj.T * makehomogeneous( obj.p3D ) );
         end
         
@@ -57,22 +63,45 @@ classdef CCube < CPattern
         function lines = getProjectionLines( obj, SimCamera )
             [~, uv_pixels] = SimCamera.projectPattern( obj );
             x = makehomogeneous( uv_pixels );
-            lines.x = { snormalize( cross( x(:,1),x(:,2) ) ),...
-                        snormalize( cross( x(:,4),x(:,7) ) ),...
-                        snormalize( cross( x(:,3),x(:,5) ) )};
-            lines.y = { snormalize( cross( x(:,1),x(:,3) ) ),...
-                        snormalize( cross( x(:,4),x(:,6) ) ),...
-                        snormalize( cross( x(:,2),x(:,5) ) )};
-            lines.z = { snormalize( cross( x(:,1),x(:,4) ) ),...
-                        snormalize( cross( x(:,2),x(:,7) ) ),...
-                        snormalize( cross( x(:,3),x(:,6) ) )};
+            K = SimCamera.K;
+            x = K \ x;
+            
+%             lines = cell(3,2,2);
+            lines.x = cell(2,2);
+            lines.y = cell(2,2);
+            lines.z = cell(2,2);
+            % X lines
+            for i=1:2
+                for j=1:2
+                    x1 = x(:,sub2ind([2,2,2],1,i,j));
+                    x2 = x(:,sub2ind([2,2,2],2,i,j));
+                    lines.x{i,j} = snormalize(cross(x1,x2));
+                end
+            end
+            % Y lines
+            for i=1:2
+                for j=1:2
+                    x1 = x(:,sub2ind([2,2,2],j,1,i));
+                    x2 = x(:,sub2ind([2,2,2],j,2,i));
+                    lines.y{i,j} = snormalize(cross(x1,x2));
+                end
+            end
+            % Z lines
+            for i=1:2
+                for j=1:2
+                    x1 = x(:,sub2ind([2,2,2],i,j,1));
+                    x2 = x(:,sub2ind([2,2,2],i,j,2));
+                    lines.z{i,j} = snormalize(cross(x1,x2));
+                end
+            end
+            
         end
         
         % Pattern 3D representation
         function h = plot3( obj ) % Plot trihedron in 3D space
-            for i=1:3
-                obj.face{i}.plot3;
-            end
+%             for i=1:3
+%                 obj.face{i}.plot3;
+%             end
             h = plot3( obj.p3D(1,:), obj.p3D(2,:), obj.p3D(3,:), '*' );
         end
     end
