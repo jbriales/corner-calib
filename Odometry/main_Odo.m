b9b157741e3334aaaac423e3d0ff26b7999288c6
Jesus Briales <jesusbriales@gmail.com>
2014-12-02 23:31:05 +0100

    Ideal cube (8 points) and computation with Elqursh and Trihedron

--- Odometry/main_Odo.m
+++ Odometry/main_Odo.m
@@ -16,7 +16,7 @@ Cam = CSimCamera( R_w_c, t_w_c, K, res, f, cam_sd );
 cube = CCube( 1, eye(3), zeros(3,1) );
 
 % Plot scene
-% cube.plotScene(Cam);
+cube.plotScene(Cam);
 
 % Compute projection of points and lines
 % [uv_proj, uv_pixels] = cube.getProjection( Cam );
@@ -24,9 +24,21 @@ lines = cube.getProjectionLines( Cam );
 keyboard % For final check
 
 % Solve with Elqursh
-v2 = cross( lines.x{1}, lines.x{2} );
-v1 = null( [ v2' * (inv(K')*inv(K)); lines.z{1}' ] );
-r1 = K \ v1;
-r2 = K \ v2;
+v2 = snormalize( cross( lines.x{1,1}, lines.x{1,2} ) );
+v1 = null( [ v2' ; lines.y{1,1}' ] );
+r1 = v1;
+r2 = v2;
 r3 = cross(r1,r2);
 R  = [r1 r2 r3];
+% Compute all possible rotations:
+
+
+% Compute trihedron planes normals from xi parameter
+Nbp = [lines.x{1,1} lines.y{1,1} lines.z{1,1}];
+[rho, OmU, D] = computeTrihedronNormals2( Nbp );
+V_tri = zeros(3,3);
+for k=1:3
+    V_tri(:,k) = OmU{k} * rho(:,k);
+end
+% rho = chooseTrihedronNormals( rho, OmU, obj_xi, Cam.K );
+% rho = refineGenericTrihedronNormals( D, [0.1 -0.2 0.3], rho );
