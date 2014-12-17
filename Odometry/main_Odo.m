067377240835ebb203b7c9ee1b344bad93239808
Jesus Briales <jesusbriales@gmail.com>
2014-12-17 18:27:40 +0100

    Update main_Odo (although already deprecated)

--- Odometry/main_Odo.m
+++ Odometry/main_Odo.m
@@ -7,7 +7,7 @@ WITH_TRIHEDRON = true;
 % WITH_PLOT = false;
 WITH_PLOT = true;
 
-% Read camera options
+% Read camera  options
 rig_config_file = fullfile( pwd, 'rig.ini' );
 rigOpts = readConfigFile( rig_config_file );
 extractStructFields( rigOpts );
@@ -19,27 +19,28 @@ poseFactory = CRandomPoses( readConfigFile( gen_conf_F,'[Vanishing]' ) );
 
 % Create simulated camera
 % TEMPORAL: cam_sd
-cam_sd = 0.1;
+cam_sd = 0;
 Cam = CSimCamera( R_w_c, t_w_c, K, res, f, cam_sd );
 
 % Create pattern
-cube = CHalfCube( 1, eye(3), zeros(3,1) );
+Cube_sd = 0.001;
+Cube = CHalfCube( 1, eye(3), zeros(3,1), Cube_sd );
 
 % Plot scene
 if WITH_PLOT
 hF_scene = figure;
-cube.plotScene(Cam);
+Cube.plotScene(Cam);
 end
 
 % Compute projection of points and lines
-% [uv_proj, uv_pixels] = cube.getProjection( Cam );
-% lines = cube.getProjectionLines( Cam );
+% [uv_proj, uv_pixels] = Cube.getProjection( Cam );
+% lines = Cube.getProjectionLines( Cam );
 
 R_gt = R_w_c';
 
 if WITH_ELQURSH
 % Solve with Elqursh
-primitives = cube.elqursh_primitives;
+primitives = Cube.elqursh_primitives;
 Nprim = length(primitives);
 K = Cam.K;
 err_elqursh = zeros(1,length(primitives));
@@ -52,7 +53,7 @@ for k=1:Nprim
     
     if WITH_PLOT
     figure(hF_scene);
-    h_prim = cube.plot_prim( prim );
+    h_prim = Cube.plot_prim( prim );
     end
 
     prim2D = prim.project(Cam);
@@ -67,10 +68,13 @@ for k=1:Nprim
     v2 = cross( line{2}, line{3} );
     v1 = null( [ v2'*(inv(K)'*inv(K)) ; line{1}' ] );
     
-    dir1 = find(prim(1).v~=0);
-    dir2 = find(prim(2).v~=0);
-    dir3 = setdiff(1:3,[dir1 dir2]);
-    
+%     dir1 = find(prim(1).v~=0);
+%     dir2 = find(prim(2).v~=0);
+%     dir3 = setdiff(1:3,[dir1 dir2]);
+    [~,dir1] = max(abs(prim(1).v));
+    [~,dir2] = max(abs(prim(2).v));
+    dir3 = setdiff(1:3,[dir1 dir2]);    
+
     R = eye(3);
     R(:,dir1) = snormalize(K \ v1);
     R(:,dir2) = snormalize(K \ v2);
@@ -127,7 +131,7 @@ end
 
 if WITH_TRIHEDRON
 % Compute with Trihedron method
-primitives = cube.trihedron_primitives;
+primitives = Cube.trihedron_primitives;
 Nprim = length(primitives);
 
 K = Cam.K;
@@ -153,7 +157,7 @@ for k=1:Nprim
     
     if WITH_PLOT
     figure(hF_scene);
-    h_prim = cube.plot_prim( prim );
+    h_prim = Cube.plot_prim( prim );
     end
 
     prim2D = prim.project(Cam);
