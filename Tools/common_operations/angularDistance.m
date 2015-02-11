1f9902d0710cc86a839ed964f35ca5121fd37123
Jesus Briales <jesusbriales@gmail.com>
2015-02-11 12:35:41 +0100

    Add default option to angularDistance for rotations to compare to identity

--- Tools/common_operations/angularDistance.m
+++ Tools/common_operations/angularDistance.m
@@ -6,5 +6,9 @@ function d = angularDistance( R1, R2 )
 % angle can always be chosen such that 0 ≤ θ ≤ π, if necessary by reversing the direction of the axis.
 % [Rotation Averaging, 4 Distance Measures on SO(3), Richard Hartley]
 
+if nargin == 1
+    R2 = eye(3);
+end
+
 chordal_distance = norm( R1-R2, 'fro' );
 d = 2 * asind( chordal_distance / (2*sqrt(2)) );
